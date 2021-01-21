f40875c50f0926794f1bca6dd49c024319b39dda
nyacity1 <74576472+nyacity1@users.noreply.github.com>
2021-01-20 20:51:45 -0500

    JavaSript

---
+++ post.js
@@ -0,0 +1,17 @@
+//name,location, age, likes, emojies, time,
+
+
+
+
+let post = {
+    name: "Kaleb Burd",
+    message: "@coderhino why is your code hot garbage?",
+    location:"Wilmington",
+    age: 25,
+    likes: 20,
+}
+post.name = "Millicent Ogutu";
+//post.message=location;
+post ['location'] = "Delaware";
+
+    console.log(post) ;
